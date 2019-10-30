381a5abcf5d2fbe1d9f26d80182ef7ffb60e16e1
Boulanov, Oleg <oleg.boulanov@hms.com>
2019-10-30 11:01:22 -0400

    Renamed package/namespace to UselessExtras.ScheduleLib

--- ScheduleLib/DateTimeSpan.cs
+++ ScheduleLib/DateTimeSpan.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScheduleLib
+namespace UselessExtras.ScheduleLib
 {
     public class DateTimeSpan
     {
--- ScheduleLib_Tests/Schedule_Tests.cs
+++ ScheduleLib_Tests/Schedule_Tests.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 using NUnit.Framework;
 
-using ScheduleLib;
+using UselessExtras.ScheduleLib;
 
 namespace ScheduleLib_Tests
 {
